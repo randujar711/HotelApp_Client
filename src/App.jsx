5486a8f79cd798417ee5615fe8506184d653b6d4
Shivani Jetty <shivanijetty@gmail.com>
2023-01-06 09:52:23 -0500

    added patch request

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 //import data from './data.json'
-import {
+import { 
   MapContainer,
   TileLayer,
   Marker,
@@ -8,6 +8,7 @@ import {
 } from "react-leaflet";
 import 'leaflet/dist/leaflet.css'
 import { hotels } from './Hotels.json'
+import React from 'react';
 import './App.css'
 
 const center = [40.8116, -73.9465]   
@@ -16,6 +17,9 @@ function App() {
 
   const [data, setData] = useState(hotels)
   const [popInfo, setPopInfo] = useState([])
+  const [open, setOpen] = useState(false)
+  const [available, setAvailable] = useState([])
+  const [name, setName] = useState([])
 
   useEffect(()=> {
     const request = async() => {
@@ -31,38 +35,82 @@ function App() {
      setPopInfo([...popInfo, info])
   }
   console.log(popInfo)
+  
+  // Creating a reservation Form
+  const handleRessy = (e) => {
+    e.preventDefault()
+
+    const getRooms = async () => {
+      let req = await fetch('http://127.0.0.1:3000/hotels/available')
+      let res = await req.json()
+      console.log(res)
+      return res
+    }
+    getRooms()
+
+       
+    const newRessy = async () => {
+      let req = await fetch('http://127.0.0.1:3000/hotels/available', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          available: available - 1         
+        })
+      })
+      setRessy((currentState) => {
+        console.log("updated!")
+        return([...currentState, {available}])
+      })
+    }
+    newRessy()
+  }
+  const handleOpen = () => {
+    setOpen(!open)
+  }
+  
   return (
     <div className="cont" >
 
     {/* ----  Menu-slider ---- */}
-      <input type="checkbox" name="" id="check"/>
+      <input className="form-menu" type="checkbox" name="" id="check"/>
       <div className="menu-container" style={{zIndex: '1000', height: '95%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
         <label for="check">
           <span className="menu fas fa-times" id="times"></span>
           <span className="menu fa-solid fa-bars" id="bar"></span>
         </label>
         {
-          popInfo.map((hotel) => {
-            {/* {const coordinates = hotel.coordinates.map(())} */}
-            return(
-              <>
-                <div style={{width: '100%', height: '35%', background: 'red'}}>
-                  {/* <img src={hotel.image} alt="" /> */}
+          <div className="parent-ressy-div">
+            <form onSubmit={(e)=>handleRessy(e)}>
+              {
+                <div className="children-ressy-div">
+                  <h2 className="h2-fonts">Book Your Trip!</h2>
+                  <input className="input-field" type="text" onChange={(e) => { e.target.value }} placeholder="Full Name" /><br />
+                  <label>Start date:
+                    <input className="input-field" onChange={(e) => { e.target.value }} type="date" value="2023-01-07" /><br />
+                  </label>
+                  <label>End date:</label>
+                  <input className="input-field" onChange={(e) => { e.target.value }} type="date" value="2023-01-07" /><br />
+                  <select className="input-field" onChange={(e) => { setName(e.target.value) }}>
+                    <option value="disabled selected">choose a hotel</option>
+                    <option value="1st">Motel 9 3/4</option>
+                    <option value="2nd">The Shrieking Shack</option>
+                    <option value="3rd">The SlytherInn</option>
+                    <option value="4th">The Leaky Cauldron</option>
+                    <option value="5th">Wizard's Way</option>
+                    <option value="6th">Hagrid's Quality Hut</option>
+                    <option value="7th">the 4 Broomsticks</option>
+                    <option value="8th">the Night Bus</option>
+                  </select>                  
+                  <div className="submit-btn-div">
+                    <input className="submit-button" type="submit" />
+                  </div>
                 </div>
-                <div style={{width: '100%', height: '55%', background: 'blue', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                  <ul style={{padding: '0'}}>
-                    <li>{hotel.name}</li>
-                    <li>{hotel.address}</li>
-                    <li>{hotel.rooms_available}</li>
-                    <li>{hotel.price}</li>
-                    <li>{hotel.rooms_available}</li>
-                    <li>{hotel.broom_rating}</li>
-                  </ul>
-                </div>
-              </>
-              
-            )
-          })
+              }
+            </form>
+          </div>
         }
       </div>
     {/* ----  Menu-slider Ends---- */}
@@ -83,7 +131,7 @@ function App() {
                 </div>
                 <ul>
                   <li>{hotel.address}</li>
-                  <li>{hotel.rooms_available}</li>
+                  <li>{hotel.available}</li>
                   <li>{hotel.name}</li>
                 </ul>
               </>
