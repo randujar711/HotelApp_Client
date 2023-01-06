0d4debd53be453eab440d321f02f85aabf4ee93d
Ryan Andujar <randujar711@gmail.com>
2023-01-06 16:28:56 -0500

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ function App() {
   const [data, setData] = useState([])
   const [posts, setPosts] = useState([])
 
+  // const [open, setOpen] = useState(false)
+  const [rooms_available, setRooms_available] = useState(0)
+  const [name, setName] = useState([])
+  const [selectedHotel, setSelectedHotel] = useState({})
+  const [ressy, setRessy] = useState([])
+
   useEffect(()=> {
     let ws;
     const request = async() => {
@@ -35,6 +41,11 @@ function App() {
           } else {
             alert(data.message.notification.message)
           }
+
+          setRessy((currentState) => {
+            console.log("updated!")
+            return ([...currentState, { rooms_available }])
+          })
         }
     }
     
@@ -58,16 +69,45 @@ function App() {
 
    const delHotel = (info) => {
     setPopInfo((prevState)=> {
-      return [...prevState.filter((x)=> {return x.id !== info.id})]
+      return [...prevState.filter((x)=> x.id !== info.id)]
     })
   }
-
+ 
+  // Creating a reservation Form
+  const handleRessy = (e) => {
+    e.preventDefault()
+        let x = rooms_available - 1
+        console.log("rooms:", rooms_available)
+    // const getRooms = async () => {
+    //   let req = await fetch(`http://127.0.0.1:3000/hotels/${selectedHotel}/rooms_available`)
+    //   let res = await req.json()
+    //   console.log(res)
+    //   return res
+    // }
+    // getRooms()
+    const newRessy = async () => {
+  
+    
+      let req = await fetch(`http://127.0.0.1:3000/hotels/1`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name
+        })
+      })
+    }
+    newRessy()
+    console.log(ressy)
+  }
 
   return (
     
     <div className="cont" >
-      <Slider delHotel={delHotel} popInfo={popInfo} />
-      <Map PopUpModal={PopUpModal} popInfo={popInfo} setPopInfo={setPopInfo} addHotel={addHotel} notifyPopup={notifyPopup} posts={posts}/>
+    {console.log(ressy)}
+      <Slider handleRessy={handleRessy} delHotel={delHotel} popInfo={popInfo} setName={setName} setRessy={setRessy} selectedHotel={selectedHotel} notifyPopup={notifyPopup}/>
+      <Map PopUpModal={PopUpModal} popInfo={popInfo} setPopInfo={setPopInfo} addHotel={addHotel} posts={posts} delHotel={delHotel}/>
 
     </div>
   )
